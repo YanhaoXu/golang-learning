bd4b02b3d18e53466a97cc81b2e2c25b45075bb3
yanhao.xu <yanhao.xu@outlook.com>
2021-12-11 21:30:02 +0800

    feature(get programming with go): chapter06
    source add

---
+++ GetProgrammingWithGo/chapter06/default.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var price64 float64
+	fmt.Println(price64)
+
+	var price32 float32
+	fmt.Println(price32)
+}
---
+++ GetProgrammingWithGo/chapter06/float.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	thrid := 1.0 / 3.0
+	fmt.Println(thrid + thrid + thrid)
+	piggyBank := 0.1
+	piggyBank += 0.2
+	fmt.Println(piggyBank)
+}
