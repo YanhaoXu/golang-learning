96a2e35944c9bd3eb31b5c2ddda66aee381533fe
yanhao.xu <yanhao.xu@outlook.com>
2023-01-02 12:45:57 +0800

    feature(golang-IM-System): add source ServerV0.3-用户消息广播

--- GolangStudy/golang-IM-System/server.go
+++ GolangStudy/golang-IM-System/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"sync"
@@ -30,8 +31,8 @@ func NewServer(ip string, port int) *Server {
 	return server
 }
 
-// ListenMessager 监听Message广播消息channel的goroutine，一但有消息就发送给全部的在线User
-func (this *Server) ListenMessager() {
+// ListenMessage 监听Message广播消息channel的goroutine，一但有消息就发送给全部的在线User
+func (this *Server) ListenMessage() {
 	for {
 		msg := <-this.Message
 
@@ -44,29 +45,51 @@ func (this *Server) ListenMessager() {
 	}
 }
 
+// BroadCast 广播消息的方法
 func (this *Server) BroadCast(user *User, msg string) {
-	sendMsg := fmt.Sprintf("[%s]%s:%s", user.Addr, user.Name, msg)
-	this.Message <- sendMsg
+	this.Message <- fmt.Sprintf("[%s]%s:%s", user.Addr, user.Name, msg)
 }
 
 func (this *Server) Handler(conn net.Conn) {
 	//...当前链接的业务
-	log.Println("链接建立成功...")
+	log.Println("Successful link establishment...")
 
-	user := NewUser(conn)
-	//用户上线,将用户加入到onlineMap中
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
+	user := NewUser(conn, this)
+
+	user.Online()
+
+	// 接受客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				user.Offline()
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				log.Println("Conn Read err:", err)
+				return
+			}
+
+			// 提取用户的消息（去除'\n'）
+			msg := string(buf[:n-1])
+
+			// 用户针对msg进行消息处理
+			user.DoMessage(msg)
+		}
+	}()
 
 	// 广播当前用户上线的消息
-	this.BroadCast(user, "已上线")
+	this.BroadCast(user, "Already online")
 
 	select {}
 }
 
 // Start 启动服务器的窗口
 func (this *Server) Start() {
+	// socket listen
 	listen, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
 		log.Println("net.Lister err:", err)
@@ -75,7 +98,7 @@ func (this *Server) Start() {
 	defer listen.Close()
 
 	// 启动监听Message的goroutine
-	go this.ListenMessager()
+	go this.ListenMessage()
 
 	for {
 		//accept
--- GolangStudy/golang-IM-System/user.go
+++ GolangStudy/golang-IM-System/user.go
@@ -7,10 +7,12 @@ type User struct {
 	Addr string
 	C    chan string
 	conn net.Conn
+
+	server *Server
 }
 
 // NewUser 创建一个用户的API
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 
 	user := &User{
@@ -18,10 +20,41 @@ func NewUser(conn net.Conn) *User {
 		Addr: userAddr,
 		C:    make(chan string),
 		conn: conn,
+
+		server: server,
 	}
+
+	// 启动监听当前user channel消息的goroutine
 	go user.ListenMessage()
 
 	return user
+}
+
+// Online 用户的上线业务
+func (this *User) Online() {
+	// 用户上线，将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	this.server.BroadCast(this, "user already online.")
+}
+
+// Offline 用户的下线业务
+func (this *User) Offline() {
+	// 用户下线，将用户从onlineMap中删除
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	// 广播当前用户下线消息
+	this.server.BroadCast(this, "user already offline.")
+}
+
+// DoMessage 用户处理消息的业务
+func (this *User) DoMessage(msg string) {
+	this.server.BroadCast(this, msg)
 }
 
 // ListenMessage 监听当前的User channel的方法，一但有消息，就直接发送给对方的客户端
